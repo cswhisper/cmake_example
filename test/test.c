163dc312539b9886c7c50e3caaa39c312da85607
Fred Bloggs <fred@bloggs.com>
2017-07-13 01:26:22 +0100

    Added an example of building multiple targets from a single source file with different preprocessor definitions.

--- test/test.c
+++ test/test.c
@@ -3,8 +3,15 @@
 #include <lib1/lib1.h>
 #include <lib2/lib2.h>
 
+#ifdef BUILD_WITH_LIB3
+    #include <lib3/lib3.h>
+#endif
+
 int main( void )
 {
     printf("%d %d\n", Lib1_test(), Lib2_test());
+#ifdef BUILD_WITH_LIB3
+    printf("%d\n", Lib3_test());
+#endif
     return 0;
 }
